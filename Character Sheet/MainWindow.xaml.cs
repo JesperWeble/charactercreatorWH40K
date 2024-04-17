de100517d59c953b9f5d65edffd0f0e7400e7106
Jesper Weble <jesperweble@outlook.com>
2024-04-17 09:15:15 +0200

    - Added Load Button and textblocks for Name, Race and Class in the xaml.
    - Added method for the Load Button that changes the text for the above textblocks when a character is loaded, in the xaml.cs

--- Character Sheet/MainWindow.xaml.cs
+++ Character Sheet/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Character_Sheet
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
         private void Load(object sender, RoutedEventArgs e)
@@ -45,49 +45,11 @@ namespace Character_Sheet
                 Character loadedChar = JsonConvert.DeserializeObject<Character>(json);
                 MessageBox.Show("Load Complete");
 
-                TextBox_CharName.Text = loadedChar.Title;
-                try
-                {
-                    raceBox.SelectedItem = raceBox.Items.Cast<Race>().FirstOrDefault(r => r.Title == loadedChar.Race.Title);
-                }
-                catch { }
-                try
-                {
-                    subraceBox.SelectedItem = subraceBox.Items.Cast<Subrace>().FirstOrDefault(r => r.Title == loadedChar.Subrace.Title);
-                }
-                catch { }
-
-                try
-                {
-                    classBox.SelectedItem = classBox.Items.Cast<Class>().FirstOrDefault(r => r.Title == loadedChar.Class.Title);
-                }
-                catch { }
-
-                strScore.Text = loadedChar.Str.ToString();
-                dexScore.Text = loadedChar.Dex.ToString();
-                conScore.Text = loadedChar.Con.ToString();
-                intScore.Text = loadedChar.Int.ToString();
-                wisScore.Text = loadedChar.Wis.ToString();
-                chaScore.Text = loadedChar.Cha.ToString();
-
-                charDesc.Text = loadedChar.Description;
-                charStory.Text = loadedChar.Backstory;
-
-                foreach (var eachSkill in loadedChar.proficiencies)
-                {
-                    string skillName = eachSkill.Key;
-                    string checkBoxName = $"checkBox_Prof_{skillName}";
-                    CheckBox checkBoxBeingChecked = (CheckBox)FindName(checkBoxName);
-                    if (eachSkill.Value == true)
-                    {
-                        checkBoxBeingChecked.IsChecked = true;
-                    }
-                    else
-                    {
-                        checkBoxBeingChecked.IsChecked = false;
-                    }
-                }
+                try { textBlock_CharacterName.Text = loadedChar.Title; } catch { }
+                try { textBlock_CharacterRace.Text = loadedChar.Race.Title; } catch { }
+                try { textBlock_CharacterClass.Text = loadedChar.Class.Title; } catch { }
 
             }
         }
+    }
 }
